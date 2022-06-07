418d023aa3811e6714ea257ef8608b25aa73ccf9
hangangqiang <hangangqiang2@huawei.com>
2022-06-07 14:42:06 +0800

    manager st by algorithm & try import from models

--- tests/st/quantization/test_sim_qat.py
+++ tests/st/quantization/test_sim_qat.py
@@ -12,104 +12,105 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""test lenet."""
+"""test qat."""
 
-from collections import OrderedDict
 import os
+import sys
+from collections import OrderedDict
 import pytest
-from mindspore_gs.quantization.simulated_quantization import SimulatedQuantizationAwareTraining
-from mindspore_gs.quantization.simulated_quantization.simulated_fake_quantizers import SimulatedFakeQuantizerPerLayer, \
-    SimulatedFakeQuantizerPerChannel
-from mindspore_gs.quantization.quantize_wrapper_cell import QuantizeWrapperCell
-import mindspore.dataset as ds
-import mindspore.dataset.vision.c_transforms as CV
-import mindspore.dataset.transforms.c_transforms as C
-from mindspore.dataset.vision import Inter
-from mindspore.common import dtype as mstype
+import mindspore
 from mindspore import nn
-from mindspore.common.initializer import Normal
-from mindspore.compression.quant.quant_utils import load_nonquant_param_into_quant_net
 from mindspore.train.serialization import load_checkpoint
 from mindspore.train.callback import ModelCheckpoint, CheckpointConfig
 from mindspore.train import Model
 from mindspore.nn.metrics import Accuracy
+from mindspore_gs.quantization.simulated_quantization import SimulatedQuantizationAwareTraining
+from mindspore_gs.quantization.simulated_quantization.simulated_fake_quantizers import SimulatedFakeQuantizerPerLayer, \
+    SimulatedFakeQuantizerPerChannel
+from mindspore_gs.quantization.quantize_wrapper_cell import QuantizeWrapperCell
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../models/official/cv/'))
 
 
-def create_dataset(data_path, batch_size=32, repeat_size=1,
-                   num_parallel_workers=1):
+class NetToQuant(nn.Cell):
     """
-    create dataset for train or test
-    """
-    # define dataset
-    mnist_ds = ds.MnistDataset(data_path)
-
-    resize_height, resize_width = 32, 32
-    rescale = 1.0 / 255.0
-    shift = 0.0
-    rescale_nml = 1 / 0.3081
-    shift_nml = -1 * 0.1307 / 0.3081
-
-    # define map operations
-    resize_op = CV.Resize((resize_height, resize_width), interpolation=Inter.LINEAR)  # Bilinear mode
-    rescale_nml_op = CV.Rescale(rescale_nml, shift_nml)
-    rescale_op = CV.Rescale(rescale, shift)
-    hwc2chw_op = CV.HWC2CHW()
-    type_cast_op = C.TypeCast(mstype.int32)
-
-    # apply map operations on images
-    mnist_ds = mnist_ds.map(operations=type_cast_op, input_columns="label", num_parallel_workers=num_parallel_workers)
-    mnist_ds = mnist_ds.map(operations=resize_op, input_columns="image", num_parallel_workers=num_parallel_workers)
-    mnist_ds = mnist_ds.map(operations=rescale_op, input_columns="image", num_parallel_workers=num_parallel_workers)
-    mnist_ds = mnist_ds.map(operations=rescale_nml_op, input_columns="image", num_parallel_workers=num_parallel_workers)
-    mnist_ds = mnist_ds.map(operations=hwc2chw_op, input_columns="image", num_parallel_workers=num_parallel_workers)
-
-    # apply DatasetOps
-    buffer_size = 10000
-    mnist_ds = mnist_ds.shuffle(buffer_size=buffer_size)  # 10000 as in LeNet train script
-    mnist_ds = mnist_ds.batch(batch_size, drop_remainder=True)
-    mnist_ds = mnist_ds.repeat(repeat_size)
-
-    return mnist_ds
-
-
-class LeNet5(nn.Cell):
-    """
-    Lenet network
-
-    Args:
-        num_class (int): Number of classes. Default: 10.
-        num_channel (int): Number of channels. Default: 1.
-
-    Returns:
-        Tensor, output tensor
-    Examples:
-        >>> LeNet5(num_class=10)
-
+    Network with single conv2d to be quanted
     """
 
-    def __init__(self, num_class=10, num_channel=1):
-        super(LeNet5, self).__init__()
-        self.conv1 = nn.Conv2d(num_channel, 6, 5, pad_mode='valid')
-        self.conv2 = nn.Conv2d(6, 16, 5, pad_mode='valid')
-        self.relu = nn.ReLU()
-        self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Dense(16 * 5 * 5, 120, weight_init=Normal(0.02))
-        self.fc2 = nn.Dense(120, 84, weight_init=Normal(0.02))
-        self.fc3 = nn.Dense(84, num_class, weight_init=Normal(0.02))
+    def __init__(self):
+        super(NetToQuant, self).__init__()
+        self.conv = nn.Conv2d(5, 6, 5, pad_mode='valid')
 
     def construct(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.max_pool2d(x)
-        x = self.conv2(x)
-        x = self.relu(x)
-        x = self.max_pool2d(x)
-        x = self.flatten(x)
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.conv(x)
         return x
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_set_config():
+    """
+    Feature: DefaultQuantAwareTraining algorithm set functions.
+    Description: Apply DefaultQuantAwareTraining on lenet.
+    Expectation: Apply success and coordinate attributes are same as config.
+    """
+
+    network = NetToQuant()
+    qat = SimulatedQuantizationAwareTraining()
+    qat.set_act_quant_delay(900)
+    qat.set_weight_quant_delay(900)
+    qat.set_act_per_channel(False)
+    qat.set_weight_per_channel(True)
+    qat.set_act_narrow_range(False)
+    qat.set_weight_narrow_range(False)
+    qat.set_one_conv_fold(True)
+    qat.set_bn_fold(False)
+    new_network = qat.apply(network)
+    cells: OrderedDict = new_network.name_cells()
+
+    assert cells.get("Conv2dQuant", None) is not None
+    conv_quant: QuantizeWrapperCell = cells.get("Conv2dQuant")
+    assert isinstance(conv_quant, QuantizeWrapperCell)
+    conv_handler = conv_quant._handler
+    weight_fake_quant: SimulatedFakeQuantizerPerChannel = conv_handler.fake_quant_weight
+    assert isinstance(weight_fake_quant, SimulatedFakeQuantizerPerChannel)
+    assert weight_fake_quant._symmetric
+    assert weight_fake_quant._quant_delay == 900
+    act_fake_quant = conv_quant._output_quantizer
+    assert isinstance(act_fake_quant, SimulatedFakeQuantizerPerLayer)
+    assert not act_fake_quant._symmetric
+    assert act_fake_quant._quant_delay == 900
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_config_enable_fusion():
+    """
+    Feature: set_enable_fusion api of DefaultQuantAwareTraining.
+    Description: Check default value of enable_fusion and value after called set_enable_fusion.
+    Expectation: Config success.
+    """
+    qat = SimulatedQuantizationAwareTraining()
+    assert not qat._config.enable_fusion
+    qat.set_enable_fusion(True)
+    assert qat._config.enable_fusion
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_config_one_conv_fold():
+    """
+    Feature: set_one_conv_fold api of DefaultQuantAwareTraining.
+    Description: Check default value of one_conv_fold and value after called set_one_conv_fold.
+    Expectation: Config success.
+    """
+    qat = SimulatedQuantizationAwareTraining()
+    assert qat._config.one_conv_fold
+    qat.set_one_conv_fold(False)
+    assert not qat._config.one_conv_fold
 
 
 @pytest.mark.level0
@@ -117,11 +118,12 @@ class LeNet5(nn.Cell):
 @pytest.mark.env_onecard
 def test_lenet():
     """
-    Feature: DefaultQuantAwareTraining algorithm.
-    Description: Apply DefaultQuantAwareTraining on lenet.
+    Feature: Simulated quantization algorithm.
+    Description: Apply simulated_quantization on lenet.
     Expectation: Apply success.
     """
 
+    from lenet.src.lenet import LeNet5
     network = LeNet5(10)
     qat = SimulatedQuantizationAwareTraining({"per_channel": [False, True], "symmetric": [False, True],
                                               "quant_delay": [900, 900]})
@@ -163,11 +165,14 @@ def test_lenet_accuracy(mnist_path_option, lenet_ckpt_path_option):
     Description: Apply sim qat on lenet5 and test accuracy.
     Expectation: accuracy is larger than 0.98.
     """
+
+    from lenet.src.lenet import LeNet5
+    from lenet.src.dataset import create_dataset as create_mnist_ds
     mnist_path = mnist_path_option
     if mnist_path_option is None:
         mnist_path = os.getenv("DATASET_PATH", "/home/workspace/mindspore_dataset/mnist")
     data_path = os.path.join(mnist_path, "train")
-    ds_train = create_dataset(data_path, 32, 1)
+    ds_train = create_mnist_ds(data_path, 32, 1)
     step_size = ds_train.get_dataset_size()
     network = LeNet5(10)
 
@@ -177,7 +182,7 @@ def test_lenet_accuracy(mnist_path_option, lenet_ckpt_path_option):
         ckpt_path = os.path.join(os.getenv("CHECKPOINT_PATH", "/home/workspace/mindspore_ckpt"),
                                  "ckpt/checkpoint_lenet-10_1875.ckpt")
     param_dict = load_checkpoint(ckpt_path)
-    load_nonquant_param_into_quant_net(network, param_dict)
+    mindspore.load_param_into_net(network, param_dict)
 
     # convert network to quantization aware network
     qat = SimulatedQuantizationAwareTraining({"per_channel": [False, True], "symmetric": [False, True],
@@ -202,7 +207,7 @@ def test_lenet_accuracy(mnist_path_option, lenet_ckpt_path_option):
                 dataset_sink_mode=True)
     print("============== End Training ==============")
 
-    ds_eval = create_dataset(os.path.join(mnist_path, "test"), 32, 1)
+    ds_eval = create_mnist_ds(os.path.join(mnist_path, "test"), 32, 1)
 
     print("============== Starting Testing ==============")
     acc = model.eval(ds_eval, dataset_sink_mode=True)
