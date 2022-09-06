ea50ed7c493aa3e73e290608de56d9bd6c84a74d
hangangqiang <hangangqiang2@huawei.com>
2022-09-06 09:16:18 +0800

    update version number

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class BuildPy(build_py):
 
 setup(
     name='mindspore_gs',
-    version='0.1.0',
+    version='0.2.0',
     author='The MindSpore Authors',
     author_email='contact@mindspore.cn',
     description='A MindSpore model optimization algorithm set..',
