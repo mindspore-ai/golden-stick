1f42b701d566433020af3c15c062d15dbf7180ed
hangangqiang <hangangqiang2@huawei.com>
2024-01-15 11:23:01 +0800

    add wikitextdataset

--- mindspore_gs/datasets/__init__.py
+++ mindspore_gs/datasets/__init__.py
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Copyright 2023 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,16 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+"""
+MindSpore golden stick datasets.
+"""
 
-cur_dir=$(cd "$(dirname $0)"; pwd)
-
-if [[ $# -ne 2 && $# -ne 3 ]]; then
-  echo "Please set preprocessed wikitext-2 dataset path like: bash quant_and_eval.sh /path/to/wiki.test.tokens /path/to/tokenizer.model."
-fi
-
-config_file="${cur_dir}/llama2_70b_2l_1p.yaml"
-ds_path=$1
-vocab_path=$2
-device_id=${3-0}
-
-python llama2_w8a16.py --config_path $config_file --device_id $device_id --dataset_path $ds_path --tokenizer_path=$vocab_path
+from .wikitext2 import create_wikitext_dataset
