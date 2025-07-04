0d10634d60ccef9179aae38cd07a4404874fb021
hangangqiang <hangangqiang2@huawei.com>
2025-07-04 22:22:12 +0800

    fix deepseek3 gptq online-weight-split bug

--- example/deepseekv3/deepseekv3_weight_processor.py
+++ example/deepseekv3/deepseekv3_weight_processor.py
@@ -1482,7 +1482,10 @@ class DeepseekV3WeightProcessor(BaseWeightProcessor):
         no_need_split_layer = ["tok_embeddings", "norm", "q2l_proj",
                                "kv2l", "routed_experts.router.dense",
                                "routed_experts.router.e_score_correction_bias",
-                               "topk_bias"]
+                               "topk_bias", ".feed_forward.w2._layer.matmul.quant_bias",
+                               ".feed_forward.w2._layer.matmul.dequant_scale",
+                               ".feed_forward.shared_experts.w2._layer.matmul.quant_bias",
+                               ".feed_forward.shared_experts.w2._layer.matmul.dequant_scale"]
 
         for param_name, _ in tqdm(hf_weight_map.items(), desc="split safetensors"):
             if "model.layers" in param_name and int(param_name.split('.')[2]) >= num_layers:
@@ -1494,13 +1497,20 @@ class DeepseekV3WeightProcessor(BaseWeightProcessor):
             elif any([name in param_name for name in [".l2q_proj.", ".feed_forward.w_gate_hidden.",
                                                       "shared_experts.w_gate_hidden"]]):
                 value, is_int4 = self.get_safetensor_from_file(param_name, src_hf_dir,
-                                                                hf_weight_map, is_split_param=True,
-                                                                split_axis=1)
-            elif any([name in param_name for name in [".feed_forward.w2.", ".wo.",
-                                                      "shared_experts.w2"]]):
+                                                               hf_weight_map, is_split_param=True,
+                                                               split_axis=1)
+            elif any([name in param_name for name in [".feed_forward.w2.quant_op.input_scale",
+                                                      ".feed_forward.w2.quant_op.input_zp",
+                                                      ".feed_forward.shared_experts.w2.quant_op.input_scale",
+                                                      ".feed_forward.shared_experts.w2.quant_op.input_zp",
+                                                      ".wo."]]):
                 value, is_int4 = self.get_safetensor_from_file(param_name, src_hf_dir,
-                                                                hf_weight_map, is_split_param=True,
-                                                                split_axis=0)
+                                                               hf_weight_map, is_split_param=True,
+                                                               split_axis=0)
+            elif any([name in param_name for name in [".feed_forward.w2.", "shared_experts.w2"]]):
+                value, is_int4 = self.get_safetensor_from_file(param_name, src_hf_dir,
+                                                               hf_weight_map, is_split_param=True,
+                                                               split_axis=1)
             elif ".routed_experts.ffn.w_gate_hidden." in param_name:
                 value, is_int4 = self.get_safetensor_from_file(param_name, src_hf_dir, hf_weight_map)
                 value_list = []
