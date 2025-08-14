2f57edfff46e4c210f4b998b76c24a7488b72fbd
yyyyrf <rifanyou@huawei.com>
2025-08-14 10:43:38 +0800

    [feature]save descrile json

--- mindspore_gs/ptq/models/mindformers_models/qwen3.py
+++ mindspore_gs/ptq/models/mindformers_models/qwen3.py
@@ -14,7 +14,7 @@
 # ============================================================================
 """qwen3 quant model"""
 
-
+from mindspore.nn.cell import Cell
 from mindspore_gs.ptq.models.mindformers_models.mf_model import MFModel
 
 
@@ -25,25 +25,64 @@ class QWen3(MFModel):
     def _split_route_moe_weight(self, param_dict) -> dict:
         return param_dict
 
-    def _process_params_dict_before_save(self, param_dict) -> dict:
-        new_param_dict = {}
+    def _convert_name(self, key):
+        """_convert_name"""
         def replacer(string, src, dst):
             if src in string:
                 string = string.replace(src, dst)
             return string
+        new_key = key
+        new_key = replacer(new_key, "._layer.matmul.", ".")
+        new_key = replacer(new_key, "._layer.", ".")
+        new_key = replacer(new_key, ".matmul.", ".")
+        new_key = replacer(new_key, ".quant_op.", ".")
+        new_key = replacer(new_key, ".input_zp", ".input_offset")
+        new_key = replacer(new_key, ".weight_zp", ".weight_offset")
+        new_key = replacer(new_key, ".dequant_scale", ".deq_scale")
+        return new_key
 
+    def _process_params_dict_before_save(self, param_dict) -> dict:
+        """_process_params_dict_before_save"""
+        new_param_dict = {}
         for key, param in param_dict.items():
             if "key_cache" in key or "value_cache" in key or "float_weight" in key:
                 continue
-            new_key = key
-            new_key = replacer(new_key, "._layer.matmul.", ".")
-            new_key = replacer(new_key, "._layer.", ".")
-            new_key = replacer(new_key, ".matmul.", ".")
-            new_key = replacer(new_key, ".quant_op.", ".")
-            new_key = replacer(new_key, ".input_zp", ".input_offset")
-            new_key = replacer(new_key, ".weight_zp", ".weight_offset")
-            new_key = replacer(new_key, ".dequant_scale", ".deq_scale")
+            new_key = self._convert_name(key)
             new_param_dict[new_key] = param
         new_param_dict = self._split_route_moe_weight(new_param_dict)
         # merge weights by TP
         return new_param_dict
+
+    def get_description_file(self, network):
+        """
+        Obtain the description of quantization type for each parameter in each layer of the network.
+        Such as W8A8 or W4A8_DYNAMIC
+        """
+        from mindspore_gs.ptq.utils import QuantType
+        if not isinstance(network, Cell):
+            raise TypeError(f"Input network should be a Cell, but got: {type(Cell)}.")
+        results = {}
+        def process(root: Cell, name_prefix):
+            """Iterate the whole network and call callback function `process_cell`."""
+            if root is None:
+                return
+            for name, cell in root.name_cells().items():
+                full_cell_name = f"{name_prefix}.{name}"
+                if not hasattr(cell, "quant_type_dict"):
+                    process(cell, full_cell_name)
+                    continue
+                info = cell.quant_type_dict()
+                for key, value in info.items():
+                    new_key = self._convert_name(key)
+                    results[new_key] = value
+                results.update(info)
+        process(network, 'network')
+
+        desc_info = {}
+        param_dict = self.parameters_dict()
+        for key, _ in param_dict.items():
+            if key in results.keys():
+                desc_info[key] = results[key]
+            else:
+                desc_info[key] = QuantType.FLOAT.value
+        return desc_info
